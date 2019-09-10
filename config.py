78ec5a10ff24595f216ce8fdf6ce82f3d10a2078
Avik Datta <reach4avik@yahoo.com>
2019-09-10 15:43:59 +0100

    updated default configs

--- config.py
+++ config.py
@@ -1,4 +1,30 @@
-## Config
+import os,uuid
+
+BASEDIR = os.path.dirname(__file__)
 
 class Config:
-  pass
+  SECRET_KEY = os.environ.get('SECRET_KEY') or uuid.uuid4().hex()         # default uuid, if not provided
+
+  @staticmethod
+  def init_app(app):
+    pass
+
+class DevConfig(Config):
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI') or \
+                            os.path.join(BASEDIR,'dev_database.sqlite')
+
+class TestConfig(Config):
+  SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URI') or \
+                            os.path.join(BASEDIR,'dev_database.sqlite')
+
+class ProdConfig(Config):
+  SQLALCHEMY_DATABASE_URI = os.environ.get('PROD_DATABASE_URI') or None
+
+class CloudConfiig(ProdConfig):
+  USE_RECAPTCHA = True                                                     # use reCAPTCHA on cloud
+
+config = {'development':DevConfig,
+          'testing':TestConfig,
+          'production':ProdConfig,
+          'cloud':CloudConfiig
+          }
