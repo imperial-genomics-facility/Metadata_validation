c4556e14334b3ac1a62a48179ca9ef6680804f36
Avik Datta <reach4avik@yahoo.com>
2019-09-12 15:15:06 +0100

    addded views for metadata and samplesheet reformatting

--- app/metadata/views.py
+++ app/metadata/views.py
@@ -1,6 +1,75 @@
+import io,os,logging
 from . import metadata
-from flask import render_template
+from flask import render_template,flash,Response,request
+from flask_wtf import FlaskForm
+from wtforms.fields import FileField,SubmitField,MultipleFileField
+from flask_wtf.file import FileAllowed,FileRequired
+from werkzeug.utils import secure_filename
+from igf_data.utils.fileutils import get_temp_dir,remove_dir
+from igf_data.process.metadata_reformat.reformat_metadata_file import Reformat_metadata_file,EXPERIMENT_TYPE_LOOKUP,SPECIES_LOOKUP,METADATA_COLUMNS
 
-@metadata.route('/')
+class MetadataForm(FlaskForm):
+  metadata_file = \
+    FileField(\
+      'Metadata csv file',
+      validators=[FileAllowed(['csv']),FileRequired()])
+  submit = SubmitField('Reformat metadata')
+
+
+def convert_file_to_stream(infile):
+  try:
+    data = ''
+    with open(infile,'r') as file_i:
+      with io.StringIO() as file_o:
+        for line in file_i:
+          file_o.write(line)
+
+        data = file_o.getvalue()
+    return data
+  except Exception as e:
+    raise ValueError('Failedto convert file to stream, error: {0}'.format(e))
+
+@metadata.route('/',methods=['GET','POST'])
 def metadata_home():
-  return render_template('metadata/metadata_reformat.html')
+  try:
+    form = MetadataForm()
+    if form.validate_on_submit():
+      temp_dir = get_temp_dir()
+      metadata_filename = \
+        secure_filename(form.metadata_file.data.filename)
+      form.metadata_file.\
+        data.save(\
+          os.path.join(\
+            temp_dir,
+            metadata_filename))
+      new_metadata_file = \
+        os.path.join(\
+          temp_dir,
+          metadata_filename)
+      metadata_output = \
+        os.path.join(\
+          temp_dir,
+          'reformatted_metadata.csv')
+      re_metadata = \
+        Reformat_metadata_file(\
+          infile=new_metadata_file,
+          experiment_type_lookup=EXPERIMENT_TYPE_LOOKUP,
+          species_lookup=SPECIES_LOOKUP,
+          metadata_columns=METADATA_COLUMNS)
+      re_metadata.\
+        reformat_raw_metadata_file(output_file=metadata_output)
+      csv_data = convert_file_to_stream(infile=metadata_output)
+      remove_dir(temp_dir)
+      return \
+        Response(\
+               csv_data,
+               mimetype="text/csv",
+               headers={"Content-disposition":
+                        "attachment; filename=reformatted_metadata.csv"})
+    else:
+      if request.method=='POST':
+        flash('Failed file type validation check')
+  except Exception as e:
+    logging.warning('Failed metadata reformatting, error: {0}'.format(e))
+
+  return render_template('metadata/metadata_reformat.html',form=form)
--- app/samplesheet/views.py
+++ app/samplesheet/views.py
@@ -1,6 +1,73 @@
-from flask import render_template
 from . import samplesheet
+import io,os,logging
+from flask import render_template,flash,Response,request
+from flask_wtf import FlaskForm
+from wtforms.fields import FileField,SubmitField,MultipleFileField
+from flask_wtf.file import FileAllowed,FileRequired
+from werkzeug.utils import secure_filename
+from igf_data.utils.fileutils import get_temp_dir,remove_dir
+from igf_data.process.metadata_reformat.reformat_samplesheet_file import Reformat_samplesheet_file
 
-@samplesheet.route('/')
+class SamplesheetForm(FlaskForm):
+  samplesheet_file = \
+    FileField(\
+      'Samplesheet csv file',
+      validators=[FileAllowed(['csv']),FileRequired()])
+  submit = SubmitField('Reformat samplesheet')
+
+
+def convert_file_to_stream(infile):
+  try:
+    data = ''
+    with open(infile,'r') as file_i:
+      with io.StringIO() as file_o:
+        for line in file_i:
+          file_o.write(line)
+
+        data = file_o.getvalue()
+    return data
+  except Exception as e:
+    raise ValueError('Failedto convert file to stream, error: {0}'.format(e))
+
+@samplesheet.route('/',methods=['GET','POST'])
 def samplesheet_home():
-  return render_template('samplesheet/samplesheet_reformat.html')
+  try:
+    form = SamplesheetForm()
+    if form.validate_on_submit():
+      temp_dir = get_temp_dir()
+      samplesheet_filename = \
+        secure_filename(form.samplesheet_file.data.filename)
+      form.samplesheet_file.\
+        data.save(\
+          os.path.join(\
+            temp_dir,
+            samplesheet_filename))
+      new_samplesheet_file = \
+        os.path.join(\
+          temp_dir,
+          samplesheet_filename)
+      samplesheet_output = \
+        os.path.join(\
+          temp_dir,
+          'reformatted_samplesheet.csv')
+      re_samplesheet = \
+        Reformat_samplesheet_file(\
+          infile=new_samplesheet_file)
+      re_samplesheet.\
+      reformat_raw_samplesheet_file(\
+        output_file=samplesheet_output)
+      csv_data = \
+        convert_file_to_stream(infile=samplesheet_output)
+      return \
+        Response(\
+               csv_data,
+               mimetype="text/csv",
+               headers={"Content-disposition":
+                        "attachment; filename=reformatted_samplesheet.csv"})
+    else:
+      if request.method=='POST':
+        flash('Failed file type validation check')
+  except Exception as e:
+    logging.warning('Failed samplesheet reformatting, error: {0}'.format(e))
+
+  return render_template('samplesheet/samplesheet_reformat.html',form=form)
