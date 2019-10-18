6f3b4ccd6644d58f61050e3ee60346b9210ba095
Avik Datta <reach4avik@yahoo.com>
2019-10-18 13:39:01 +0100

    updated test for covcal view

--- test/covcalculator_view_test.py
+++ test/covcalculator_view_test.py
@@ -10,9 +10,8 @@ class CovcalculatorView_test(unittest.TestCase):
     db.drop_all()
     db.create_all()
     self.client = self.app.test_client(use_cookies=True)
-    platform_data = \
-      {'id':1,
-       'name':'HiSeq 4000 50 SR',
+    platform_data_list = [\
+      {'name':'HiSeq 4000 50 SR',
        'm_reads':270,
        'm_bases':13500,
        'm_clusters':312,
@@ -21,13 +20,29 @@ class CovcalculatorView_test(unittest.TestCase):
        'lanes':8,
        'max_samples':96,
        'is_pe':0,
-       'read_length':50}
-    platform = Platform(platform_data)
-    db.session.add(platform)
+       'read_length':50},
+       {'name':'HiSeq 4000 150 PE',
+       'm_reads':270,
+       'm_bases':8100,
+       'm_clusters':312,
+       'm_output':93600,
+       'clusters':312500000,
+       'lanes':8,
+       'max_samples':96,
+       'is_pe':1,
+       'read_length':150}]
+    platform_lists =\
+       [Platform(**platform_data) 
+          for platform_data in platform_data_list]
+    db.session.add_all(platform_lists)
+    db.session.commit()
+
   def tearDown(self):
     db.session.remove()
     db.drop_all()
     self.app_context.pop()
   
+  def test1(self):
+    print([i.name for i in Platform.query.all()])
 if __name__ == '__main__':
   unittest.main()
